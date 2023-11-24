df522a8129ca624e5284e56eef5090c2780f43cb
Maciej Necka <macieknecka@gmail.com>
2023-11-24 11:55:06 +0100

    Calendar list - sorted added, Expired added

--- src/components/CalendarList.js
+++ src/components/CalendarList.js
@@ -2,11 +2,29 @@ import React from 'react';
 
 class CalendarList extends React.Component {
   render() {
+    const currentDateTime = new Date();
+
+    const sortedMeetings = this.props.meetings.sort((a, b) => {
+      const dateA = new Date(`${a.date} ${a.time}`);
+      const dateB = new Date(`${b.date} ${b.time}`);
+      return dateA - dateB;
+    });
+
+    const upcomingMeetings = sortedMeetings.filter((meeting) => {
+      const meetingDateTime = new Date(`${meeting.date} ${meeting.time}`);
+      return meetingDateTime > currentDateTime;
+    });
+
+    const expiredMeetings = sortedMeetings.filter((meeting) => {
+      const meetingDateTime = new Date(`${meeting.date} ${meeting.time}`);
+      return meetingDateTime <= currentDateTime;
+    });
+
     return (
       <div className="calendar-list">
         <h1 className="calendar-list__title">Meeting List</h1>
         <ul className="calendar-list__items">
-          {this.props.meetings.map((meeting) => (
+          {upcomingMeetings.map((meeting) => (
             <li key={meeting.id} className="calendar-list__item">
               <div className="calendar-list__header">
                 <span className="calendar-list__date">
@@ -59,6 +77,65 @@ class CalendarList extends React.Component {
             </li>
           ))}
         </ul>
+        {expiredMeetings.length > 0 && (
+          <div>
+            <h2 className="calendar-list__title">Expired Meetings</h2>
+            <ul className="calendar-list__items">
+              {expiredMeetings.map((meeting) => (
+                <li key={meeting.id} className="calendar-list__item expired">
+                  <div className="calendar-list__header">
+                    <span className="calendar-list__date">
+                      <span
+                        className="calendar-list__icon"
+                        role="img"
+                        aria-label="Calendar"
+                      >
+                        📅
+                      </span>
+                      {meeting.date}
+                    </span>
+                    <br />
+                    <span className="calendar-list__time">
+                      <span
+                        className="calendar-list__icon"
+                        role="img"
+                        aria-label="Clock"
+                      >
+                        🕒
+                      </span>
+                      {meeting.time}
+                    </span>
+                  </div>
+                  <div className="calendar-list__details">
+                    <span className="calendar-list__name">
+                      {' '}
+                      <span
+                        className="calendar-list__icon"
+                        role="img"
+                        aria-label="Person"
+                      >
+                        👤
+                      </span>
+                      {meeting.firstName} {meeting.lastName}
+                    </span>
+                    <br />
+                    <span className="calendar-list__email">
+                      <span
+                        className="calendar-list__icon"
+                        role="img"
+                        aria-label="Email"
+                      >
+                        ✉️
+                      </span>
+                      {meeting.email}
+                    </span>
+                    <br />
+                  </div>
+                </li>
+              ))}
+            </ul>
+          </div>
+        )}
       </div>
     );
   }
