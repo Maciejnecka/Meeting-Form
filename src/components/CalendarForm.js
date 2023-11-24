4f561f9f86cb03b164bf5dc83383043e0226befd
Maciej Necka <macieknecka@gmail.com>
2023-11-24 10:07:46 +0100

    validateFrom added

--- src/components/CalendarForm.js
+++ src/components/CalendarForm.js
@@ -10,7 +10,7 @@ class CalendarForm extends React.Component {
       email: '',
       date: '',
       time: '',
-      error: '',
+      errors: {},
     };
   }
 
@@ -20,34 +20,88 @@ class CalendarForm extends React.Component {
 
   handleSubmit = (e) => {
     e.preventDefault();
-    if (!this.validateForm()) {
-      return;
+    const errors = this.validateForm();
+    if (Object.keys(errors).length === 0) {
+      const id = uuidv4();
+      const newMeeting = {
+        id,
+        firstName: this.state.firstName,
+        lastName: this.state.lastName,
+        email: this.state.email,
+        date: this.state.date,
+        time: this.state.time,
+      };
+
+      this.props.onSubmit(newMeeting);
+
+      this.setState({
+        firstName: '',
+        lastName: '',
+        email: '',
+        date: '',
+        time: '',
+        errors: {},
+      });
+    } else {
+      this.setState({ errors });
     }
-    const id = uuidv4();
-    const newMeeting = {
-      id,
-      firstName: this.state.firstName,
-      lastName: this.state.lastName,
-      email: this.state.email,
-      date: this.state.date,
-      time: this.state.time,
-    };
-
-    this.props.onSubmit(newMeeting);
-
-    this.setState({
-      firstName: '',
-      lastName: '',
-      email: '',
-      date: '',
-      time: '',
-      error: '',
-    });
   };
 
   validateForm = () => {
-    // temp
-    return true;
+    const errors = {};
+    const fields = [
+      {
+        name: 'firstName',
+        label: 'First name',
+        required: true,
+        pattern: '^[a-zA-Z -]+$',
+        minLength: 2,
+      },
+      {
+        name: 'lastName',
+        label: 'Last name',
+        required: true,
+        pattern: '^[a-zA-Z -]+$',
+        minLength: 2,
+      },
+      {
+        name: 'email',
+        label: 'Email',
+        required: true,
+        pattern: /^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}$/,
+      },
+      {
+        name: 'date',
+        label: 'Date',
+        required: true,
+        pattern: /^\d{4}-\d{2}-\d{2}$/,
+      },
+      {
+        name: 'time',
+        label: 'Time',
+        required: true,
+        pattern: /^([01]\d|2[0-3]):([0-5]\d)$/,
+      },
+    ];
+
+    fields.forEach((field) => {
+      const value = this.state[field.name];
+
+      if (field.required && value.trim() === '') {
+        errors[field.name] = `${field.label} is required.`;
+      }
+
+      if (field.minLength && value.length < field.minLength) {
+        errors[
+          field.name
+        ] = `${field.label} should be at least ${field.minLength} characters long.`;
+      }
+
+      if (field.pattern && !new RegExp(field.pattern).test(value)) {
+        errors[field.name] = `${field.label} is not in the correct format.`;
+      }
+    });
+    return errors;
   };
 
   render() {
@@ -56,37 +110,45 @@ class CalendarForm extends React.Component {
         <h1 className="form__title">Meeting Form</h1>
         <form className="form__container" onSubmit={this.handleSubmit}>
           <label className="form__label">
-            First Name
             <input
+              placeholder="First name"
               className="form__input"
               type="text"
               name="firstName"
               value={this.state.firstName}
               onChange={this.handleInputChange}
             />
+            {this.state.errors.firstName && (
+              <p className="form__error">{this.state.errors.firstName}</p>
+            )}
           </label>
           <label className="form__label">
-            Last Name
             <input
+              placeholder="Last name"
               className="form__input"
               type="text"
               name="lastName"
               value={this.state.lastName}
               onChange={this.handleInputChange}
             />
+            {this.state.errors.lastName && (
+              <p className="form__error">{this.state.errors.lastName}</p>
+            )}
           </label>
           <label className="form__label">
-            Email{' '}
             <input
+              placeholder="Email"
               className="form__input"
               type="email"
               name="email"
               value={this.state.email}
               onChange={this.handleInputChange}
             />
+            {this.state.errors.email && (
+              <p className="form__error">{this.state.errors.email}</p>
+            )}
           </label>
           <label className="form__label">
-            Date{' '}
             <input
               className="form__input"
               type="date"
@@ -94,9 +156,11 @@ class CalendarForm extends React.Component {
               value={this.state.date}
               onChange={this.handleInputChange}
             />
+            {this.state.errors.date && (
+              <p className="form__error">{this.state.errors.date}</p>
+            )}
           </label>
           <label className="form__label">
-            Time{' '}
             <input
               className="form__input"
               type="time"
@@ -104,12 +168,14 @@ class CalendarForm extends React.Component {
               value={this.state.time}
               onChange={this.handleInputChange}
             />
+            {this.state.errors.time && (
+              <p className="form__error">{this.state.errors.time}</p>
+            )}
           </label>
           <button className="form__button" type="submit">
             Submit
           </button>
         </form>
-        {this.state.error && <p className="form__error">{this.state.error}</p>}
       </div>
     );
   }
